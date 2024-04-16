02daacd54f7ddad6af0fbeb50c419b1dc3162861
Antoine <antoine.paillard@mousquetaires.com>
2024-04-16 10:44:07 +0200

    Create Model :  Rdv

--- Gestion_File_Attente/app/models.py
+++ Gestion_File_Attente/app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Rdv(models.Model):
+    date = models.DateField()
+    time = models.TimeField()
+    entreprise = models.CharField()
+
+    def __str__(self):
+        return self.entreprise
