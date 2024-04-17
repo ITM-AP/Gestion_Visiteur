d9dc5d3a2a4f6470d493cad9f1276024ddaf135b
Antoine <antoine.paillard@mousquetaires.com>
2024-04-17 17:46:39 +0200

    Update

--- Gestion_File_Attente/app/static/js/script.js
+++ Gestion_File_Attente/app/static/js/script.js
@@ -1,4 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
+  function afficherDateHeure() {
+    // Obtiens la date actuelle
+    var date = new Date();
+    
+    // Obtient les éléments de l'heure
+    var heures = date.getHours();
+    var minutes = date.getMinutes();
+    
+    // Ajoute un zéro en tête si les minutes ou les secondes sont inférieures à 10
+    minutes = (minutes < 10 ? "0" : "") + minutes;
+    
+    // Crée une chaîne représentant l'heure actuelle
+    var heureActuelle = heures + ":" + minutes;
+    
+    // Affiche l'heure dans un élément HTML avec l'ID "horloge"
+    document.getElementById("horloge").innerText = heureActuelle;
+  }
+
+  // Met à jour l'heure toutes les secondes (1000 millisecondes)
+  afficherDateHeure()
+  setInterval(afficherDateHeure, 1000);
+  
+
   const rdv = document.querySelectorAll(".rdv");
 
   rdv.forEach((icon) => {
@@ -28,13 +51,11 @@ document.addEventListener("DOMContentLoaded", function () {
           "Content-Type": "application/json",
           "X-CSRFToken": getCookie("csrftoken"),
         },
-        body: JSON.stringify({
-          rdv_id: rdvId,
-        }),
+        body: JSON.stringify({rdv_id: rdvId})
       })
         .then((response) => response.json())
         .then((data) => {
-          if (data.succes) {
+          if (data.success) {
             location.reload();
           } else {
             console.error(data.error);
--- Gestion_File_Attente/app/views.py
+++ Gestion_File_Attente/app/views.py
@@ -7,31 +7,38 @@ import json
 # Create your views here.
 
 def app_view(request):
-    if request.method == 'GET':
-        today = date.today()
-        rdvs = Rdv.objects.all().order_by('date', 'time')
-        form = RdvForm()
-        return render(request, 'index.html', {'rdvs': rdvs})
-    elif request.method == 'POST':
+    rdvs = Rdv.objects.all().order_by('date', 'time')
+    form = RdvForm()
+    if request.method == 'POST':
         form = RdvForm(request.POST)
-        form.save()
         if form.is_valid():
+            form.save()
             return redirect('app_view')
+        # Si le formulaire n'est pas valide, rend le formulaire avec les erreurs
+        return render(request, 'index.html', {'rdvs': rdvs})
+    # Si la méthode est GET ou si le formulaire n'est pas valide, affiche tous les rendez-vous
+    return render(request, 'index.html', {'rdvs': rdvs})
 
 def delete_rdv(request):
     if request.method == 'POST':
-        data = json.load(request)
-        rdv_id = data.get('rdv_id')
         try:
-            rdv = Rdv.objects.get(id=rdv_id)
-            rdv.delete()
-            return JsonResponse({'succes': True})
+            data = json.load(request)
+            rdv_id = data.get('rdv_id')
+            if rdv_id is not None:
+                rdv = Rdv.objects.get(id=rdv_id)
+                rdv.delete()
+                return JsonResponse({'success': True})
+            else:
+                return JsonResponse({'success': False, 'error': 'Identifiant dfe rendez-vous manquant.'})
+        except json.JSONDecodeError:
+            return JsonResponse({'success': False, 'error': 'Erreur lors de la lecture des données JSON.'})
         except Rdv.DoesNotExist:
-            return JsonResponse({'succes': False, 'error': 'Le rendez-vous n\'existe pas.'})
-
+            return JsonResponse({'success': False, 'error': 'Le rendez-vous n\'existe pas.'})
+    else:
+        return JsonResponse({'success': False, 'error': 'La méthode HTTP doit être POST.'})
+    
 def afficheur_view(request):
     if request.method == 'GET':
         today = date.today()
         rdvs = Rdv.objects.all().filter(date=today).order_by('time')
-        form = RdvForm()
         return render(request, 'afficheur.html', {'rdvs': rdvs})
